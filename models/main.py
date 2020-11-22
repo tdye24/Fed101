4cd5b363f376d85f80e98bec479f7d29a9a01aaf
tdye24 <18365225454@163.com>
2020-11-22 03:30:08 +0000

    add: main.py

---
+++ models/main.py
@@ -0,0 +1,20 @@
+import os
+from models.utils.model_utils import read_data
+from models.server import Server
+from models.client import Client
+
+
+# setup clients
+def setup_clients(dataset, model=None):
+    train_data_dir = os.path.join('..', 'data', dataset, 'data', 'train')
+    test_data_dir = os.path.join('..', 'data', dataset, 'data', 'test')
+
+    users, train_data, test_data = read_data(train_data_dir, test_data_dir)
+    clients_ = [Client(user_id, train_data[user_id], test_data[user_id], model, mini_batch=0.1) for user_id in users]
+    return clients_
+
+
+if __name__ == '__main__':
+    clients = setup_clients('femnist', model='cnn')
+    server = Server(clients, rounds=3000, clients_per_round=3)
+    server.federate()
