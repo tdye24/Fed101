4c6475792416b0440aa782f2a5127855debdf09d
tdye24 <18365225454@163.com>
2020-12-05 03:43:57 +0000

    update client.py

--- models/client.py
+++ models/client.py
@@ -1,13 +1,26 @@
 import torch
+import sys
 import torch.nn.functional as F
 import torch.optim as optim
+
+# 模型
 from models.femnist.cnn import Net
-from models.femnist.resnet import ResNet, Basicblock
+from models.cifar10.resnet import Resnet
+from models.synthetic_iid.fcnn import FCNN
+from models.mnist.MNIST2NN import MNIST2NN
+from models.mnist.MNISTCNN import MNISTCNN
+
 from models.utils.model_utils import batch_data
+import inspect
+from models.utils.gpu_mem_track import MemTracker
+
+frame = inspect.currentframe()  # define a frame to track
+gpu_tracker = MemTracker(frame)  # define a GPU tracker
 
 
 class Client:
-    def __init__(self, user_id, train_data, test_data, model_name: str, lr=3e-4, batch_size=10, mini_batch=-1, epoch=1, seed=123):
+    def __init__(self, user_id, train_data, test_data, model_name: str, lr=3e-4, batch_size=10, mini_batch=-1, epoch=1,
+                 seed=123):
         self.user_id = user_id
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.train_data = train_data
@@ -23,19 +36,26 @@ class Client:
         self.loss_list = []
         self.acc_list = []
 
-    # TODO (self.model移到init函数)
     def select_model(self, model_name):
         model = None
         if model_name == 'cnn':
             model = Net()
         elif model_name == 'resnet':
-            model = ResNet(Basicblock, [1, 1, 1, 1], 62)
+            model = Resnet()
+        elif model_name == 'fcnn':
+            model = FCNN()
+        elif model_name == 'mnist2nn':
+            model = MNIST2NN()
+        elif model_name == 'mnistcnn':
+            model = MNISTCNN()
         else:
             print("Unimplemented Model!")
             exit(0)
-        self.model = model.to(self.device)
+        self.model = model
 
     def train(self):
+        # gpu_tracker.track()
+        self.model = self.model.to(self.device)
         self.model.train()
         # batch大小，可以指定batch_size，也可以指定mini_batch
         if self.mini_batch == -1:
@@ -43,35 +63,46 @@ class Client:
         else:
             frac = min(1.0, self.mini_batch)
             num_data = max(1, int(frac * len(self.train_data['y'])))
-        BEST_ACC = 0
         for i in range(self.epoch):
             for batched_x, batched_y in batch_data(self.train_data, num_data, self.seed):
                 input_data, target_data = self.process_data(batched_x, batched_y)
+                if len(batched_y) <= 1:
+                    continue
                 self.optimizer.zero_grad()
                 output = self.model(input_data)
                 loss = self.criterion(output, target_data)
                 loss.backward()
-                self.loss_list.append(loss)
+                self.loss_list.append(loss.item())
                 self.optimizer.step()
-                # acc = self.test()
-                # if acc > BEST_ACC:
-                #     BEST_ACC = acc
-        # print("user: {}, acc: {}, best acc: {}".format(self.user_id, acc, BEST_ACC))
+        self.model = self.model.cpu()
+
         num_train_samples, update = len(self.train_data['y']), self.get_params()
+
+        torch.cuda.empty_cache()
+        # gpu_tracker.track()
         return num_train_samples, update
 
     def test(self):
-        self.model.eval()
-        test_data_x = torch.tensor(self.test_data['x']).to(self.device)
-        test_data_y = torch.tensor(self.test_data['y']).to(self.device)
-        output = self.model(test_data_x)
-        output = torch.argmax(output, dim=-1)
-        num_test_samples = len(self.test_data['y'])
-        acc = torch.sum(output == test_data_y).float() / len(test_data_y)
+        # gpu_tracker.track()
+        with torch.no_grad():
+            self.model.to(self.device)
+            self.model.eval()
+            test_data_x = torch.tensor(self.test_data['x']).to(self.device)
+            test_data_y = torch.tensor(self.test_data['y']).to(self.device)
+            output = self.model(test_data_x)
+            output = torch.argmax(output, dim=-1)
+
+            num_test_samples = len(self.test_data['y'])
+            acc = float(torch.sum(output == test_data_y).float() / len(test_data_y))
+
+            self.model.cpu()
+            test_data_x.cpu(), test_data_y.cpu(), output.cpu()
+            torch.cuda.empty_cache()
+        # gpu_tracker.track()
         return num_test_samples, acc
 
     def process_data(self, raw_x, raw_y):
-        input_data, target_data = torch.tensor(raw_x).to(device=self.device), torch.tensor(raw_y).to(device=self.device)
+        input_data, target_data = torch.tensor(raw_x).float().to(device=self.device), torch.tensor(raw_y).long().to(device=self.device)
         return input_data, target_data
 
     def get_params(self):
