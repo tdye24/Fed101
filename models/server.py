aee8ba861b4aa562a01a5fe68de1cc5e3c5b909d
tdye24 <18365225454@163.com>
2021-01-27 06:00:08 +0000

    update: server.py

--- models/server.py
+++ models/server.py
@@ -1,78 +1,204 @@
 import gc
 import os
+import copy
 import sys
 import torch.multiprocessing as mp
 import torch
+from torchvision import transforms
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
-from models.utils.model_utils import read_data
+from models.utils.model_utils import read_data, compare_models, Logger
 # noinspection PyUnresolvedReferences
 from tensorboardX import SummaryWriter
 from models.client import Client
+from data.CONSTANTS import *
+from data.cifar10.CIFAR10_DATASET import get_cifar10_dataloaders
+from data.mnist.MNIST_DATASET import get_mnist_dataloaders, get_mnist_dataloaders_niid
 
 plt.switch_backend('agg')
 
-i_ = 0
-while os.path.exists('../visualization/fed' + str(i_)):
-    i_ += 1
-writer = SummaryWriter('../visualization/fed' + str(i_))
-
 
 class Server:
-    def __init__(self, model_path, partitioning='iid', seed=123, rounds=20, epoch=1, clients_per_round=1, eval_interval=1,
-                 dataset_name='femnist', model_name='cnn', lr=3e-4, batch_size=1, mini_batch=0.1):
+    def __init__(self, algorithm='fedavg', partitioning='iid', seed=123, rounds=20, epoch=1, all_clients_num=100,
+                 clients_per_round=1,
+                 eval_interval=1,
+                 dataset_name='femnist', model_name='cnn', lr=3e-4, batch_size=1, mini_batch=0.1, lr_decay=0.99,
+                 pretrain_model=None, decay_step=200):
+        self.algorithm = algorithm
+        self.clients = []
         self.seed = seed  # randomly sampling
-        self.partitioning = partitioning
-        self.clients = self.setup_clients(dataset_name, model_name=model_name, lr=lr, batch_size=batch_size,
-                                          mini_batch=mini_batch)
-        self.model_path = model_path
-        self.rounds = rounds
+        self.lr = lr
+        self.lr_decay = lr_decay
+        self.decay_step = decay_step
+        self.mini_batch = mini_batch
         self.epoch = epoch
-        self.clients_per_round = min(clients_per_round, len(self.clients))
-        if len(self.clients) > 0:
-            self.params = self.clients[0].model.state_dict()
+        self.partitioning = partitioning
+        self.model_name = model_name
+        self.rounds = rounds
+        self.batch_size = batch_size
+        self.params = None
+        self.all_clients_num = all_clients_num
+        self.clients_per_round = clients_per_round
+        self.pretrain_model = pretrain_model  # 预训练模型地址
+        self.eval_interval = eval_interval
+        self.dataset_name = dataset_name
+        self.updates = []
+        self.selected_clients = []
+        self.optim = {'round': 0, 'acc': 0.0, 'params': None, 'loss': 10e8}  # 第几轮，准确率，最高准确率对应的参数
+        self.train_writer = None
+        self.test_writer = None
+
+    def initiate(self):
+        self.clients = self.setup_clients(self.dataset_name, model_name=self.model_name, lr=self.lr,
+                                          batch_size=self.batch_size,
+                                          mini_batch=self.mini_batch)
+        assert self.clients_per_round <= len(self.clients)
+        self.clients_per_round = min(self.clients_per_round, len(self.clients))
+        if len(self.clients) > 0 and self.pretrain_model is None:
+            self.params = copy.deepcopy(self.clients[0].model.state_dict())
+        elif self.pretrain_model is not None:
+            self.load_pretrain()
         else:
             print("Error：clients长度为0")
             exit(0)
-        self.eval_interval = eval_interval
-        self.updates = []
-        self.acc_over_sub = []  # 在选中的clients上的准确率
-        self.acc_over_all = []  # 在所有clients上的准确率
-        self.selected_clients = []
-        self.optim = {'round': 0, 'acc': 0.0, 'params': None}  # 第几轮，准确率，最高准确率对应的参数
+        batch_size = self.batch_size
+        mini_batch = self.mini_batch
+        dataset_name = self.dataset_name
+        partitioning = self.partitioning
+        model_name = self.model_name
+        clients_per_round = self.clients_per_round
+        epoch = self.epoch
+
+        # 绘图初始化
+        if batch_size >= len(self.clients[0].trainloader.dataset._ids):  # 全部数据作为一个batch
+            flag = "NAN"
+        else:
+            flag = batch_size
+
+        # 画训练和测试时的acc和loss，设置两个writer，主要是想让两个曲线（训练和验证）放在一起
+        if mini_batch == -1:
+            self.train_writer = SummaryWriter(
+                f'../visualization/{self.algorithm}-{dataset_name}-{partitioning}-{model_name}-C{clients_per_round}-E{epoch}-B{flag}-train')
+            self.test_writer = SummaryWriter(
+                f'../visualization/{self.algorithm}-{dataset_name}-{partitioning}-{model_name}-C{clients_per_round}-E{epoch}-B{flag}-val')
+        else:
+            self.train_writer = SummaryWriter(
+                f'../visualization/{self.algorithm}-{dataset_name}-{partitioning}-{model_name}-C{clients_per_round}-E{epoch}-M{mini_batch}-train')
+            self.test_writer = SummaryWriter(
+                f'../visualization/{self.algorithm}-{dataset_name}-{partitioning}-{model_name}-C{clients_per_round}-E{epoch}-M{mini_batch}-val')
+
+    def load_pretrain(self):
+        print(f"loading pre-trained model from {self.pretrain_model}")
+        self.params = torch.load(self.pretrain_model)
 
     def setup_clients(self, dataset_name, model_name: str, batch_size: int, mini_batch: float, lr: float):
-        train_data_dir = os.path.join('..', 'data', dataset_name, 'data', 'train')
-        test_data_dir = os.path.join('..', 'data', dataset_name, 'data', 'test')
+        # 改成dataloader形式
+        # train_data_dir = os.path.join('..', 'data', dataset_name, 'data', self.partitioning, 'train')
+        # test_data_dir = os.path.join('..', 'data', dataset_name, 'data', self.partitioning, 'test')
+        #
+        # users, train_data, test_data = read_data(train_data_dir, test_data_dir)
+        users = []
+        trainloaders, testloaders = [], []
 
-        users, train_data, test_data = read_data(train_data_dir, test_data_dir, self.partitioning)
+        if self.dataset_name == 'cifar10':
+            users = [i for i in range(100)]
+            train_transform = transforms.Compose([
+                # transforms.RandomCrop(size=24, padding=8, fill=0, padding_mode='constant'),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.RandomApply([
+                    transforms.ColorJitter(brightness=0.8, contrast=0.8, saturation=0.8, hue=0.2)], p=0.8),
+
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+            ])
+            # train_transform = transforms.Compose([
+            #     transforms.ToTensor(),
+            #     transforms.Normalize(mean=[0.485, 0.456, 0.406],
+            #                          std=[0.229, 0.224, 0.225])
+            # ])
+
+            test_transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+            ])
+            trainloaders, testloaders = get_cifar10_dataloaders(batch_size=self.batch_size,
+                                                                num_clients=self.all_clients_num,
+                                                                train_transform=train_transform,
+                                                                test_transform=test_transform)
+        elif self.dataset_name == 'mnist':
+            if self.partitioning == 'iid':
+                users = [i for i in range(100)]
+                train_transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485],
+                                         std=[0.229])
+                ])
+
+                test_transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485],
+                                         std=[0.229])
+                ])
+                trainloaders, testloaders = get_mnist_dataloaders(batch_size=self.batch_size,
+                                                                  num_clients=self.all_clients_num,
+                                                                  train_transform=train_transform,
+                                                                  test_transform=test_transform)
+            else:
+                users = [i for i in range(100)]
+                train_transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485],
+                                         std=[0.229])
+                ])
+
+                test_transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485],
+                                         std=[0.229])
+                ])
+                trainloaders, testloaders = get_mnist_dataloaders_niid(batch_size=self.batch_size,
+                                                                       num_clients=self.all_clients_num,
+                                                                       train_transform=train_transform,
+                                                                       test_transform=test_transform)
+
         clients = [
-            Client(user_id, train_data[user_id], test_data[user_id], model_name=model_name, batch_size=batch_size,
-                   mini_batch=mini_batch, lr=lr) for user_id in users]
+            Client(user_id, trainloader=trainloaders[user_id], testloader=testloaders[user_id], model_name=model_name,
+                   batch_size=batch_size,
+                   mini_batch=mini_batch, lr=lr, epoch=self.epoch, lr_decay=self.lr_decay, decay_step=self.decay_step,
+                   algorithm=self.algorithm) for user_id in users]
         return clients
 
     def select_clients(self, round_th):
-        num_clients = min(self.clients_per_round, len(self.clients))
         np.random.seed(seed=round_th)
-        selected_clients = np.random.choice(self.clients, num_clients, replace=False)
+        selected_clients = np.random.choice(self.clients, self.clients_per_round, replace=False)
+        #   设置client的随机种子，用于shuffle data(单独给每个client设置一个seed会导致每次shuffle的结果不变)
+        for client in selected_clients:
+            client.seed = round_th
+
         self.selected_clients = selected_clients
-        # return selected_clients
 
     def average(self):
+        updates = copy.deepcopy(self.updates)
         total_weight = 0
-        new_params = OrderedDict()
-        for (client_samples, client_params) in self.updates:
+        (client_samples, new_params) = updates[0]
+
+        for (client_samples, client_params) in updates:
             total_weight += client_samples
-            for (k, v) in client_params.items():
-                if k not in new_params:
-                    new_params[k] = v * client_samples
-                else:
-                    new_params[k] += v * client_samples
+
         for k in new_params.keys():
-            new_params[k] /= total_weight
+            for i in range(0, len(updates)):
+                client_samples, client_params = updates[i]
+                # 权重
+                w = client_samples / total_weight
+                if i == 0:
+                    new_params[k] = client_params[k] * w
+                else:
+                    new_params[k] += client_params[k] * w
         # 更新global模型参数
-        self.params = new_params
+        self.params = copy.deepcopy(new_params)
 
     @staticmethod
     def avg_metric(metric_list):
@@ -92,109 +218,86 @@ class Server:
             # 该条语句不能往后放，因为接下来要测试模型在选中的clients上的性能
             self.select_clients(round_th=i)
             if i % self.eval_interval == 0:
-                # 按理来说应该只传给被选中的clients，
-                # 但是此处是为了测试global model在所有的clients上的平均准确率
+                print("--------------------------\n")
+                print("Round {}".format(i))
                 for c in self.clients:
                     c.set_params(self.params)
-                # 测试当前模型在选中clients和所有clients上的准确率
-                print("Round {}".format(i), end=' ')
-                acc_over_all = self.test(domain="all")
-                self.acc_over_all = acc_over_all
-                acc_over_sub = self.test(domain="sub")
-                self.acc_over_sub = acc_over_sub
+                acc_over_all, loss_over_all = self.test(dataset='test')
+                avg_acc_all, avg_loss_all = self.avg_metric(acc_over_all), self.avg_metric(loss_over_all)
 
-                avg_acc_all = self.avg_metric(acc_over_all)
-                avg_acc_sub = self.avg_metric(acc_over_sub)
-                # 决定是否保存当前模型为最优模型
                 if avg_acc_all > self.optim['acc']:
-                    print("***当前最优模型***SAVE***")
-                    self.optim.update({'round': i, 'acc': avg_acc_all, 'params': self.params})
+                    print("\033[1;31m" + "***当前最优模型***SAVE***" + "\033[0m")
+                    self.optim.update({'round': i, 'acc': avg_acc_all, 'params': self.params, 'loss': avg_loss_all})
                     self.save_model()
 
-                # visualization
-                writer.add_scalar('fedavg_all', avg_acc_all, global_step=i)
-                writer.add_scalar('fedavg_sub', avg_acc_sub, global_step=i)
+                self.test_writer.add_scalar('acc', avg_acc_all, global_step=i)
+                self.test_writer.add_scalar('loss', avg_loss_all, global_step=i)
 
-            # clients训练one by one
+                # 算一下在训练集上的表现
+                for c in self.clients:
+                    c.set_params(self.params)
+                acc_over_all, loss_over_all = self.test(dataset='train')
+                avg_acc_all, avg_loss_all = self.avg_metric(acc_over_all), self.avg_metric(loss_over_all)
+
+                self.train_writer.add_scalar('acc', avg_acc_all, global_step=i)
+                self.train_writer.add_scalar('loss', avg_loss_all, global_step=i)
+
             for c in self.selected_clients:
-                # TODO 之前server测试的时候将所有的client model设置为global model
-                #  此处重复设置client model（仅仅是实验需求）
-                # 每次下发最新模型
                 c.set_params(self.params)
-                # 每次下发在所有clients上的最优模型
-                # c.set_params(self.optim['params'])
-                # 在选中的clients上继续进行训练，并传回参数
-                num_train_samples, update = c.train()
-                self.updates.append((num_train_samples, update))
+                num_train_samples, update, loss = c.train(round_th=i)
+                self.updates.append((num_train_samples, copy.deepcopy(update)))
 
             # 联邦平均
             self.average()
+            # 这一步本不需要再前向传播计算一遍loss，为了代码上的统一（和test），就再计算一遍吧
 
             # 清空本轮状态
             self.updates.clear()
-            self.acc_over_all.clear()
-            self.acc_over_sub.clear()
-            # 其实也不用清空，查看是否是这条语句导致内存一直上涨
-            self.selected_clients = None
+            self.selected_clients = []
 
-    def test(self, domain: str):
+    def test(self, dataset='test'):
         """
         返回所有clients的平均准确率
+        :param dataset: 在训练集上计算loss和acc还是在测试集上计算loss和acc
         :param domain: 参数是all,平均self.clients,参数是sub,平均self.selected_clients
         :return: 准确率列表，格式[（num_samples, acc）,...]
         """
-        if domain == "all":
-            processes = []
-            for c in self.clients:
-                # p = mp.spawn(fn=c.test, nprocs=1, join=True, daemon=False)
-                # p.start()
-                # processes.append(p)
-                num_test_samples, acc = c.test()
-                self.acc_over_all.append((num_test_samples, acc))
-                torch.cuda.empty_cache()
-            # for p in processes:
-            #     p.join()
-            res_list = self.acc_over_all
-            self.acc_over_all = []
-            print("Average accuracy of all the clients: {}".format(self.avg_metric(res_list)))
-        elif domain == "sub":
-            for c in self.selected_clients:
-                num_test_samples, acc = c.test()
-                c.acc_list.append(acc)  # client维护自己训练过程中的accuracy list
-                self.acc_over_sub.append((num_test_samples, acc))
-                torch.cuda.empty_cache()
-            res_list = self.acc_over_sub
-            self.acc_over_sub = []
-            print("\t\tAverage accuracy of the selected clients: {}".format(self.avg_metric(res_list)))
-        return res_list
+        acc_list, loss_list = [], []
+
+        for c in self.clients:
+            num_test_samples, acc, loss = c.test(dataset=dataset)
+            acc_list.append((num_test_samples, acc))
+            loss_list.append((num_test_samples, loss))
+            torch.cuda.empty_cache()
+        if dataset == 'test':
+            print(
+                f"#TEST# All clients, Avg acc: {self.avg_metric(acc_list)}, Avg loss: {self.avg_metric(loss_list)}")
+        else:
+            print(
+                f"#TRAIN# All clients, Avg acc: {self.avg_metric(acc_list)}, Avg loss: {self.avg_metric(loss_list)}")
+        return acc_list, loss_list
 
     def print_optim(self):
         for c in self.clients:
             c.set_params(self.params)  # 下发最终的模型
         print("Round {}".format(self.rounds), end=' ')
-        acc_all = self.test(domain="all")
+        acc_all, loss_all = self.test(dataset='test')
         # acc_sub = self.test(domain="sub")
-        avg_acc_all = self.avg_metric(acc_all)
+        avg_acc_all, avg_loss_all = self.avg_metric(acc_all), self.avg_metric(loss_all)
         # avg_acc_sub = self.avg_metric(acc_sub)
 
         if avg_acc_all > self.optim['acc']:
-            print("***当前最优模型***SAVE***")
-            self.optim.update({'round': self.rounds, 'acc': avg_acc_all, 'params': self.params})
+            print("\033[1;31m" + "***当前最优模型***SAVE***" + "\033[0m")
+            self.optim.update({'round': self.rounds, 'acc': avg_acc_all, 'params': self.params, 'loss': avg_loss_all})
             self.save_model()
         print(
-            f"######Round: {self.optim['round']}, Optimal Federated Model, Average Accuracy Over All Clients(AAAC): {self.optim['acc']}######")
-
-    def client_info(self, user_index):
-        for i in range(len(self.clients[user_index].loss_list)):
-            writer.add_scalar('client {} loss'.format(user_index), self.clients[user_index].loss_list[i], global_step=i)
-        for i in range(len(self.clients[user_index].acc_list)):
-            writer.add_scalar('client {} acc'.format(user_index), self.clients[user_index].acc_list[i], global_step=i)
+            f"######Round: {self.optim['round']}, Optimal Federated Model, Average Accuracy Over All Clients(AAAC): "
+            f"\033[1;32m{self.optim['acc']}\033[0m######")
 
     def save_model(self):
-        path = ''
-        if not os.path.exists(self.model_path):
-            for s in self.model_path.split('/')[:-1]:
-                path = path + s + '/'
-            path = path[:-1]
+        path = f'../result/{self.dataset_name}/{self.partitioning}/{self.model_name}/C{self.clients_per_round}-E{self.epoch}-B{self.batch_size}'
+        if not os.path.exists(path):
             os.makedirs(path)
-        torch.save(self.params, self.model_path)
+        path = f'{path}/model.pkl'
+        print(f"模型已保存至：{path}")
+        torch.save(self.params, path)
