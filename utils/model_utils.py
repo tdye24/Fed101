38e451d286a99b9b20f8e7af0bc5ea56732538fe
tdye24 <18365225454@163.com>
2021-01-31 17:17:44 +0000

    add: utils

---
+++ utils/model_utils.py
@@ -0,0 +1,73 @@
+import json
+import numpy as np
+import sys
+import os
+from collections import defaultdict
+
+
+class Logger(object):
+    def __init__(self, filename="log.txt"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
+
+
+def batch_data(data, batch_size, seed):
+    data_x = data['x']
+    data_y = data['y']
+
+    # randomly shuffle data
+    np.random.seed(seed)
+    rng_state = np.random.get_state()
+    np.random.shuffle(data_x)
+    np.random.set_state(rng_state)
+    np.random.shuffle(data_y)
+
+    # loop through mini-batches
+    for i in range(0, len(data_x), batch_size):
+        batched_x = data_x[i:i + batch_size]
+        batched_y = data_y[i:i + batch_size]
+        yield (batched_x, batched_y)
+
+
+def read_dir(data_dir):
+    clients = []
+    data = defaultdict(lambda: None)
+
+    files = os.listdir(data_dir)
+    files = [f for f in files if f.endswith('.json')]
+    for f in files:
+        file_path = os.path.join(data_dir, f)
+        with open(file_path, 'r') as inf:
+            cdata = json.load(inf)
+        clients.extend(cdata['users'])
+        data.update(cdata['user_data'])
+
+    # clients = list(sorted(data.keys()))
+    return clients, data
+
+
+def read_data(train_data_dir, test_data_dir):
+    """parses data in given train and test data directories
+    assumes:
+    - the data in the input directories are .json files with
+        keys 'users' and 'user_data'
+    - the set of train set users is the same as the set of test set users
+    Return:
+        clients: list of client ids
+        groups: list of group ids; empty list if none found
+        train_data: dictionary of train data
+        test_data: dictionary of test data
+    """
+    train_clients, train_data = read_dir(train_data_dir)
+    test_clients, test_data = read_dir(test_data_dir)
+    # 可能clients读入的顺序不一样
+    assert train_clients.sort() == test_clients.sort()
+
+    return train_clients, train_data, test_data
