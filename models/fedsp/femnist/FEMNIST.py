9a1a44ed8f232b1ef53d82dd98b3da268adb2569
tdye24 <18365225454@163.com>
2021-03-01 14:23:12 +0000

    fedsp/femnist/FEMNIST add 1 cnn to clf

--- models/fedsp/femnist/FEMNIST.py
+++ models/fedsp/femnist/FEMNIST.py
@@ -6,25 +6,31 @@ class FEMNIST(nn.Module):
     def __init__(self):
         super(FEMNIST, self).__init__()
         self.global_feature = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, padding=2),
+            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2)
+            # nn.Conv2d(in_channels=8, out_channels=16, kernel_size=5, padding=2),
+            # nn.ReLU(inplace=True),
+            # nn.MaxPool2d(2, 2)
         )
 
         self.local_feature = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=8, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=5, padding=2),
+            # nn.Conv2d(in_channels=8, out_channels=16, kernel_size=5, padding=2),
+            # nn.ReLU(inplace=True),
+            # nn.MaxPool2d(2, 2)
+        )
+
+        self.clf_cnn = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2)
         )
         # 改成2048
         self.fc = nn.Sequential(
-            nn.Linear(64*7*7 + 16*7*7, 512),  # 乘2因为global_feat和local_feat拼在一起
+            nn.Linear(16*7*7*2, 512),  # 乘2因为global_feat和local_feat拼在一起
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),
             nn.Linear(512, 62)
@@ -33,10 +39,10 @@ class FEMNIST(nn.Module):
     def forward(self, x):
         global_feat = self.global_feature(x)
         local_feat = self.local_feature(x)
-        global_feat_flat = global_feat.flatten(start_dim=1)
-        local_feat_flat = local_feat.flatten(start_dim=1)
-        feature = torch.cat((global_feat_flat, local_feat_flat), dim=1)
-        output = self.fc(feature)
+        feature = torch.cat((global_feat, local_feat), dim=1)
+        clf_cnn_out = self.clf_cnn(feature)
+        clf_cnn_out_flatten = clf_cnn_out.flatten(start_dim=1)
+        output = self.fc(clf_cnn_out_flatten)
         return output
 
 
