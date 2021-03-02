5335082fc135a4652df7e94177f5acfb0acab3f0
tdye24 <18365225454@163.com>
2021-03-02 02:22:14 +0000

    observe gate output

--- models/fedmoe/femnist/FEMNIST.py
+++ models/fedmoe/femnist/FEMNIST.py
@@ -1,5 +1,7 @@
+import copy
 import torch
 import torch.nn as nn
+import numpy as np
 
 
 class FEMNIST(nn.Module):
@@ -23,42 +25,52 @@ class FEMNIST(nn.Module):
             nn.MaxPool2d(2, 2)
         )
 
-        self.GATE = nn.Sequential(
+        self.GATE_feature = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2),
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2),
+        )
+
+        self.GATE_fc = nn.Sequential(
             nn.Linear(64*7*7, 512),
             nn.ReLU(inplace=True),
             nn.Linear(512, 2),
-            nn.Softmax(dim=0)
+            nn.Softmax(dim=1)
         )
 
         # 改成2048
         self.fc = nn.Sequential(
-            nn.Linear(64*7*7, 512),  # 乘2因为global_feat和local_feat拼在一起
+            nn.Linear(64*7*7, 512),
             nn.ReLU(inplace=True),
             nn.Linear(512, 62),
         )
 
     def forward(self, x):
         global_feat = self.global_feature(x)
+        global_feat_flatten = global_feat.flatten(start_dim=1).unsqueeze(1)
         local_feat = self.local_feature(x)
-        gate_output = self.GATE(x)
-        sum_feat = gate_output[0]*global_feat + gate_output[1]*local_feat
-        print("global weight", gate_output[0])
-        print("local weight", gate_output[1])
-        feature = sum_feat.flatten(start_dim=1)
+        local_feat_flatten = local_feat.flatten(start_dim=1).unsqueeze(1)
+        feature = torch.cat((global_feat_flatten, local_feat_flatten), 1)
+        gate_feat = self.GATE_feature(x)
+        gate_feat_flatten = gate_feat.flatten(start_dim=1)
+        gate_output = self.GATE_fc(gate_feat_flatten)
+
+        avg_gate_output = torch.sum(gate_output, dim=0)/gate_output.shape[0]
+        # print(avg_gate_output)
+
+        gate_output = gate_output.unsqueeze(-1)
+        feature = torch.sum(feature * gate_output, dim=1)
         output = self.fc(feature)
-        return output
+        return output, avg_gate_output
 
 
 if __name__ == '__main__':
     model = FEMNIST()
     _x = torch.rand((50, 1, 28, 28))
-    _output = model(_x)
+    _output, avg_gate_output_ = model(_x)
     print(f'{_x.shape}->{_output.shape}')
     print("Parameters in total {}".format(sum(x.numel() for x in model.parameters())))
 
